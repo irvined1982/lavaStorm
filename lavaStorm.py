e37bb2b5b461badd3c49576e4f777b447210a3bd
David Irvine <irvined@gmail.com>
2013-10-18 15:02:16 -0600

    Added options to pass on network and file params to consume resources

--- lavaStorm.py
+++ lavaStorm.py
@@ -33,6 +33,15 @@ parser.add_argument('--min_mem', type=int, default=1)
 parser.add_argument('--max_mem', type=int, default=1)
 parser.add_argument('--min_cores', type=int, default=1)
 parser.add_argument('--max_cores', type=int, default=1)
+parser.add_argument("-n", "--network", action="store_true", default=False, help="Send data between nodes using MPI")
+parser.add_argument("-N", "--network_iterations", type=int, default=100, help="Number if iterations of the network test")
+parser.add_argument("-c", "--compute", action="store_true", default=True, help="Compute random numbers")
+parser.add_argument("-m", "--memory", type=int, default=100, help="Megabytes of RAM to consume")
+parser.add_argument("-f", "--file", action="store_true", default=False, help="Write data to files")
+parser.add_argument("-F", "--file_size", type=int, default=100, help="File size in gb")
+parser.add_argument("-d", "--directory", type=str, default="", help="Location to write files to")
+parser.add_argument("-S", "--seek_timeout", type=int, default=120, help="Perform seek operations for this long")
+
 args=parser.parse_args()
 
 r=Random()
@@ -61,6 +70,14 @@ for i in xrange(args.num_jobs):
 		command.append( '-P' )
 		command.append( r.choice( args.project ) )
 
-	command.append( 'mpirun -n %d ./consumeResources.py %d --memory %d -n -c' % (num_cores, job_runtime, mem_size ) )
+	enabled_features=""
+	if args.network:
+		enabled_features+="-n "
+	if args.file:
+		enabled_features += "-f "
+	if args.compute:
+		enabled_features += "-c "
+
+	command.append( 'mpirun -n %d ./consumeResources.py %d --memory %d %s --file_size="%s" --directory="%s" --seek_timeout="%s"' % (num_cores, job_runtime, mem_size, enabled_features, args.file_size, args.directory, args.seek_timeout) )
 	subprocess.call(command)
 
